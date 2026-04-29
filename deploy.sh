87ba13326933e42403202c1d4dd94a36fe026272
Eugene <e.viktorovvv@gmail.com>
2026-04-29 23:33:34 +0600

    Improving the project build and deployment infrastructure

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
 set -e
 
-PROJECT_NAME="mirocopy-converterbot"
-APP_DIR="$HOME/MiroCopy-ConverterBot"
+PROJECT_NAME="mirocopy-converter-bot"
+APP_DIR="$HOME/MiroCopy-Converter-Bot"
 
-echo "🚀 Deploy ConverterBot ($PROJECT_NAME)"
+echo "🚀 Deploy start"
 
-cd "$APP_DIR"
+cd "$APP_DIR" || { echo "❌ $APP_DIR not found"; exit 1; }
 
 echo "📥 Pull latest code"
 git pull
 
-echo "🛑 Stop old ConverterBot containers"
+echo "🛑 Stop old containers"
 docker compose -p "$PROJECT_NAME" down --remove-orphans
 
-echo "🔨 Build & start ConverterBot containers"
+echo "🔨 Build & start containers"
 docker compose -p "$PROJECT_NAME" up -d --build
 
 echo "🧹 Cleaning up unused Docker images..."
 docker image prune -f
 
-echo "✅ ConverterBot deploy finished"
+echo "✅ Deploy finished"
